e5ad5c82ac07788110641ee021ad2b5baa917511
Nada Anis Nurjihan <nurjinurjihan26@gmail.com>
2023-10-12 21:34:13 +0700

    commit

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def movie_post():
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = request.get(url_receive, headers=headers)
+    data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
@@ -58,8 +58,8 @@ def movie_post():
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
-    movie_list = list(db.movies.find({},{'_id':False}))
-    return jsonify({'movies':movie_list})
+    movie_list = list(db.movies.find({},{'_id': False}))
+    return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
